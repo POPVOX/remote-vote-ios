a1638c8d4378fe47b8d53fb0a42931a9e7e321b6
Jordan Kay <jordan@fleuronic.com>
2020-07-17 18:41:55 -0700

    Create Xcode project

---
+++ RemoteVote/ViewController.swift
@@ -0,0 +1,14 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
